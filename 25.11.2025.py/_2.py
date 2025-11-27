9b713d736287b1534532ef3581f89d31505770e8
Mikhail <karapetyan.7702@gmail.com>
2025-11-27 20:10:10 +0300

    обновление

---
+++ 25.11.2025.py/_2.py
@@ -0,0 +1,25 @@
+things = list()
+things.append(3)
+things.append(None)
+things.append('xyz')
+print(things)
+
+numbers = [4, 5, 6]
+print(numbers)
+
+# tuple не изменяемые 
+
+things = tuple()
+
+# У кортежа нет append 
+print(things)
+ 
+data = tuple([3, 4, 5, None, 7])
+print(data)
+
+values = 8.9, 12, -12, 0
+print(values)
+
+more_values = ("x", "y", "z")
+print(more_values)
+one_element_tuple = (1, ) 
---
+++ 25.11.2025.py/slovari.py
@@ -0,0 +1,14 @@
+tasks = {
+    55: "Состричь траву",
+    -8: "Построить дом",
+    "девять": "Забрать зарплату",
+
+}
+print(f"{tasks = }")
+
+
+for key in tasks:
+    print(key, tasks[key])
+
+print(tasks["девять"])
+
